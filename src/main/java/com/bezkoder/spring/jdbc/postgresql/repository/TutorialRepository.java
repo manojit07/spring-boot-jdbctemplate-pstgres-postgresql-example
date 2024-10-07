7eb4ac42388e5eb8128d7e69b8c8a66da8543cfe
manojit07 <manojit07@gmail.com>
2024-10-07 12:00:37 +0530

    jar commited

--- src/main/java/com/bezkoder/spring/jdbc/postgresql/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/jdbc/postgresql/repository/TutorialRepository.java
@@ -5,19 +5,19 @@ import java.util.List;
 import com.bezkoder.spring.jdbc.postgresql.model.Tutorial;
 
 public interface TutorialRepository {
-  int save(Tutorial book);
-
-  int update(Tutorial book);
-
-  Tutorial findById(Long id);
-
-  int deleteById(Long id);
-
-  List<Tutorial> findAll();
-
-  List<Tutorial> findByPublished(boolean published);
-
-  List<Tutorial> findByTitleContaining(String title);
-
-  int deleteAll();
+//  int save(Tutorial book);
+//
+//  int update(Tutorial book);
+//
+//  Tutorial findById(Long id);
+//
+//  int deleteById(Long id);
+//
+//  List<Tutorial> findAll();
+//
+//  List<Tutorial> findByPublished(boolean published);
+//
+//  List<Tutorial> findByTitleContaining(String title);
+//
+//  int deleteAll();
 }
